dd95553289407238491167de16af87abfaf7fbc0
mshah0686 <mshah0686@gmail.com>
2022-11-22 11:35:39 -0600

    Fixed compilation issue

--- examples/person_detection/main/image_processing.h
+++ examples/person_detection/main/image_processing.h
@@ -1,14 +1,19 @@
-#include <string.h>
-#include <sys/param.h>
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/event_groups.h"
-#include "esp_system.h"
-#include "esp_log.h"
-#include "esp_event.h"
 
+#ifndef IMAGE_PROCESSING_H
+#define IMAGE_PROCESSING_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void grayscale_task_setup();
 void downscale_task_setup();
 void downscale_task_loop(QueueHandle_t input_q, QueueHandle_t output_q);
 void grayscale_task_loop(QueueHandle_t input_q, QueueHandle_t output_q);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
